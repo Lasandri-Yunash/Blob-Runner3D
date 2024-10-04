be4199f9452b6657449192964f39b9ec55e55a90
Lasandri silva <lasandri.yunash@gmail.com>
2024-10-04 11:52:05 +0530

    Fixed bugs

--- Assets/_Main/Scripts/GamePlay/Player/PController.cs
+++ Assets/_Main/Scripts/GamePlay/Player/PController.cs
@@ -11,15 +11,27 @@ public class PController : MonoBehaviour
 
     [SerializeField] private float speed = 2f;  // Movement speed
     [SerializeField] private float rotationSpeed = 5f;  // Rotation speed
+
     [SerializeField] private Vector2 movementLimit = Vector2.one;  // Movement limit (X-axis)
+
+
     [SerializeField] private GameObject model = null;  // Model reference
 
     private PAnimationController _animationController = null;
     private MeshRenderer _renderer = null;
     private Player _player = null;
 
+    [SerializeField] private UIManager uiManager;
+
+
     private bool _canMove = false;  // Control movement
     private Vector2 _direction = Vector2.zero;  // Movement direction
+
+
+    private Vector3 _lastPosition;
+    private float _timeSinceLastMove = 0f;
+    private float _movementCheckDelay = 5f;
+
 
     public PlayerState currentState { get; private set; } = PlayerState.OnStandRun;
 
@@ -64,6 +76,7 @@ public class PController : MonoBehaviour
             Debug.LogError("PAnimationController component not found.");
         }
 
+        _lastPosition = transform.position;
         // Begin the coroutine to determine animation state based on broken body parts
         StartCoroutine(DetermineAnimationStateViaBrokenParts());
     }
@@ -83,7 +96,29 @@ public class PController : MonoBehaviour
         }
 
         ValidateLocation();
+        CheckIfPlayerIsMoving();
     }
+
+    private void CheckIfPlayerIsMoving()
+    {
+        if (transform.position == _lastPosition)
+        {
+            // Player hasn't moved, start counting time
+            _timeSinceLastMove += Time.fixedDeltaTime;
+
+            if (_timeSinceLastMove >= _movementCheckDelay)
+            {
+                TriggerGameOver(); // Trigger game over if player hasn't moved
+            }
+        }
+        else
+        {
+            // Player has moved, reset the timer
+            _timeSinceLastMove = 0f;
+            _lastPosition = transform.position;
+        }
+    }
+
 
     private IEnumerator DetermineAnimationStateViaBrokenParts()
     {
@@ -121,6 +156,18 @@ public class PController : MonoBehaviour
         }
     }
 
+    private void TriggerGameOver()
+    {
+        // Call the GameOver method in the UIManager to activate the game over panel
+        if (uiManager != null)
+        {
+            uiManager.ShowGameOver();
+        }
+
+        Debug.Log("Game Over: Player did not move.");
+    }
+
+
     private void UpdateState(PlayerState newState)
     {
         currentState = newState;
@@ -150,6 +197,9 @@ public class PController : MonoBehaviour
     {
         _direction = Vector2.zero;
     }
+
+   
+
 
     private void ValidateLocation()
     {
